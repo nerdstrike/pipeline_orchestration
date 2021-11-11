8d42dd24ffcce6a7be580f4e3e6a4695dc27b54f
Kieron Taylor <kt19@sanger.ac.uk>
2021-11-11 15:13:37 +0000

    Throw databases out, so we can work with ORM objects and not just pseudo-queries

--- pipeline_orchestrator/server/server.py
+++ pipeline_orchestrator/server/server.py
@@ -1,30 +1,22 @@
-import databases
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
+# also ApiRouter to get routes out of here
 from starlette import status
 from typing import List, Optional, Dict
 
-from pipeline_orchestrator.server.config import DevConfig
 from pipeline_orchestrator.server.model import Pipeline, AnalysisRun, WorkRegistrationResult
-from pipeline_orchestrator.tracking.db import DbAccessor
-
-config = DevConfig
-
-database = databases.Database(config.DB_URL)
-
-db_interface = DbAccessor(database)
-
+from pipeline_orchestrator.tracking.db import get_DbAccessor
 
 app = FastAPI()
 
 
-@app.on_event('startup')
-async def startup():
-    await database.connect()
+# @app.on_event('startup')
+# async def startup():
+#     await engine.connect()
 
 
-@app.on_event('shutdown')
-async def shutdown():
-    await database.disconnect()
+# @app.on_event('shutdown')
+# async def shutdown():
+#     await engine.dispose()
 
 
 @app.get("/")
@@ -35,17 +27,17 @@ async def root():
 
 
 @app.get("/pipeline", response_model=List[Pipeline])
-async def pipelines():
+async def get_pipelines(db_interface=Depends(get_DbAccessor)):
     return await db_interface.get_all_pipelines()
 
 
-@app.post("/pipelines")
+@app.post("/pipeline")
 async def create_pipeline():
     return None
 
 
 @app.get("/run_status", response_model=List[AnalysisRun])
-async def runs(state: Optional[str] = None):
+async def runs(state: Optional[str] = None, db_interface=Depends(get_DbAccessor)):
     return await db_interface.get_analysis_runs(state)
 
 
@@ -54,7 +46,7 @@ async def runs(state: Optional[str] = None):
     status_code=status.HTTP_201_CREATED,
     response_model=WorkRegistrationResult
 )
-async def create_analysis_run(analysis_id: str, body: Dict, agent_id: Optional[str]):
+async def create_analysis_run(analysis_id: str, body: Dict, agent_id: Optional[str], db_interface=Depends(get_DbAccessor)):
     runs = [run for run in body]
     # Some kind of validation?
     await db_interface.create_work(analysis_id, agent_id, runs)
--- pipeline_orchestrator/tracking/db.py
+++ pipeline_orchestrator/tracking/db.py
@@ -1,7 +1,27 @@
 import logging
 from sqlalchemy import select
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from typing import List
 
-from pipeline_orchestrator.tracking.schema import Pipeline, AnalysisRun
+from pipeline_orchestrator.server.config import DevConfig
+
+from pipeline_orchestrator.tracking.schema import (
+    Agent, Analysis, AnalysisRun, Pipeline
+)
+
+config = DevConfig
+engine = create_async_engine(
+    config.DB_URL, future=True
+)
+session_factory = sessionmaker(engine, expire_on_commit=False, class_=AsyncSession)
+
+
+async def get_DbAccessor():
+    'Provides a hook for fastapi to Depend on a live session on each route'
+    async with session_factory() as session:
+        async with session.begin():
+            yield DbAccessor(session)
 
 
 class DbAccessor:
@@ -11,25 +31,48 @@ class DbAccessor:
 
     logger = logging.getLogger(__name__)
 
-    def __init__(self, database):
-        self.database = database
+    def __init__(self, session):
+        self.session = session
 
     async def get_all_pipelines(self):
-        pipelines = await self.database.fetch_all(
-            query=select(Pipeline)
+        pipelines = await self.session.execute(
+            select(Pipeline)
         )
 
-        return pipelines
+        return pipelines.scalars().all()
 
-    async def get_analysis_runs(self, state):
+    async def get_analysis_runs(self, state: str):
         if (state):
-            analysis_runs = await self.database.fetch_all(
+            analysis_runs = await self.session.execute(
                 select(AnalysisRun)
                 .filter(state=state)
             )
         else:
-            analysis_runs = await self.database.fetch_all(
+            analysis_runs = await self.session.execute(
                 select(AnalysisRun)
             )
 
-        return analysis_runs
+        return analysis_runs.scalars().all()
+
+    async def create_run(self, analysis_id: str, agent_id: str, runs: List[object]):
+        async with self.session.begin():
+            # Now we have an AsyncSessionTransaction
+
+            session = self.session
+            agent = await session.execute(
+                select(Agent)
+                .where(agent_id=agent_id)
+            ).scalar_one()
+            analysis = await session.execute(
+                select(Analysis)
+                .where(analysis_id=analysis_id)
+            ).scalar_one()
+            # Check they exist and so on
+
+            for run in runs:
+                analysis_run = AnalysisRun(
+                    agent=agent,
+                    analysis=analysis,
+                    job_descriptor='blurp'
+                )
+                session.add(analysis_run)
